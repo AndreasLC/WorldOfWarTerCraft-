33994337713f5acc8fc5f81456d84e58f388ae7f
L-tech-s <joachimlow02@gmail.com>
2024-11-15 00:27:13 +0100

    Small changes

--- Game.cs
+++ Game.cs
@@ -1,44 +1,41 @@
 /* Main class for launching the game
  */
 
-class Game
-{
-    static World world = new World();
-    static Context context = new Context(world.GetBeach());
-    static ICommand fallback = new CommandUnknown();
-    static Registry registry = new Registry(context, fallback);
-
-    private static void InitRegistry()
-    {
-        ICommand cmdExit = new CommandExit();
-        registry.Register("exit", cmdExit);
-        /*why do we have 4 commands that does the same?*/
-        /*registry.Register("quit", cmdExit);*/
-        /*registry.Register("bye", cmdExit);*/
-        /*registry.Register("kill", cmdExit);*/
-        registry.Register("go", new CommandGo());
-        //registry.Register("talk", new CommandNPC());
-        registry.Register("help", new CommandHelp(registry));
-        registry.Register("inventory", new CommandInventory());
-        registry.Register("inv", new CommandInventory());
+class Game {
+  static World    world    = new World();
+  static Context  context  = new Context(world.GetBeach());
+  static ICommand fallback = new CommandUnknown();
+  static Registry registry = new Registry(context, fallback);
+  
+  private static void InitRegistry () {
+    ICommand cmdExit = new CommandExit();
+    registry.Register("exit", cmdExit);
+    registry.Register("quit", cmdExit);
+    registry.Register("bye", cmdExit);
+    registry.Register("kill", cmdExit);
+    registry.Register("go", new CommandGo());
+    //registry.Register("talk", new CommandNPC());
+    registry.Register("help", new CommandHelp(registry));
+    registry.Register("inventory", new CommandInventory()); 
+    registry.Register("inv", new CommandInventory()); 
+  }
+  
+  static void Main (string[] args) {
+   Console.WriteLine("Welcome to World of Trash!");
+   Console.WriteLine();
+   Console.WriteLine("You are the mighty sea turtle, your objective is to clean the plastic polluted sea and save the world!");
+   Console.WriteLine();
+    InitRegistry();
+    context.GetCurrent().EnterSpace();
+    
+    while (context.IsDone()==false) {
+      Console.WriteLine();
+      string? line = Console.ReadLine();
+      Console.WriteLine();
+      // Converts input from user to lowercase:)
+      line = line.ToLower();
+      if (line!=null) registry.Dispatch(line);
     }
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to Trashtopia!");
-        Console.WriteLine();
-        InitRegistry();
-        context.GetCurrent().Welcome();
-
-        while (context.IsDone() == false)
-        {
-            Console.WriteLine();
-            string? line = Console.ReadLine();
-            Console.WriteLine();
-            // Converts input from user to lowercase:)
-            line = line.ToLower();
-            if (line != null) registry.Dispatch(line);
-        }
-        Console.WriteLine("You died");
-    }
+    Console.WriteLine("You died");
+  }
 }
