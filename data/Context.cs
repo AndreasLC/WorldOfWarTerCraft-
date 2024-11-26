b07df4dc38bb13e63e6d926bf262e7549c3089f6
L-tech-s <joachimlow02@gmail.com>
2024-11-26 02:45:16 +0100

    Code cleanup + small changes

--- data/Context.cs
+++ data/Context.cs
@@ -4,11 +4,11 @@ class Context {
   Space current;
   bool done = false;
   public Inventory PlayerInventory;
-  public int PlayerHealth{get;set;}  =3; 
+  public int PlayerHealth{get;set;}  =  3; 
   
   public Context (Space node) {
-  current = node;
-  PlayerInventory = new Inventory(new List<Item>()); // Starting with an empty inventory
+    current = node;
+    PlayerInventory = new Inventory(new List<Item>()); // Starting with an empty inventory
   }
   
   public Space GetCurrent() {
@@ -20,7 +20,7 @@ class Context {
     if (next==null) {
       Console.WriteLine($"You are confused, and walk in a circle looking for '{direction}'. In the end you gave up :(");
     } else {
-            Space.Goodbye();
+      Space.Goodbye();
       current = next;
       current.EnterSpace();
     }
@@ -34,15 +34,11 @@ class Context {
     return done;
   }
   // Method for removing lifes in challenge.
-   public void RemoveLifes()
-    {
-        PlayerHealth --; // Gets context player health wich is set to 3 and deducts 1 everytime it runs. 
-        Console.WriteLine("you lost a life, you now have "+ PlayerHealth + " lives left!");  
-        if(PlayerHealth <=0) // If lifes get to 0 call method MakeDone wich execute the game. 
-        { 
-        Console.WriteLine("womp womp you lost all your lives");
-       }
+  public void RemoveLifes() {
+    PlayerHealth --; // Gets context player health wich is set to 3 and deducts 1 everytime it runs. 
+    Console.WriteLine("You lost a life, you now have "+ PlayerHealth + " lives left!");  
+    if(PlayerHealth <=0) { // If lifes get to 0 call method MakeDone wich execute the game.  
+      Console.WriteLine("You lost all your lives, better luck next time! :)");
     }
-
-
+  }
 }
