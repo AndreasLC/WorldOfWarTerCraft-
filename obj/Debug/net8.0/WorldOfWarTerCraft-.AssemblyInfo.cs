b6497507a9109350f38ce3ec39f680085416c56f
AndreasLC <andreaslykkecaspers@gmail.com>
2024-12-08 19:54:30 +0100

    removed code not in use from beginchallenge

--- obj/Debug/net8.0/WorldOfWarTerCraft-.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldOfWarTerCraft-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfWarTerCraft-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ec22f0fc2a6d24685ed0290e9580d4127813fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfWarTerCraft-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfWarTerCraft-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
